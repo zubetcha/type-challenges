be9fecc6ef6209fa13fab682d50cd11d29bce10d
zubetcha <zuhye5@gmail.com>
2023-04-03 20:12:32 +0900

    easy: Awaited

--- questions/00268-easy-if/test-cases.ts
+++ questions/00268-easy-if/test-cases.ts
@@ -1,9 +1,9 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
 
 type cases = [
-  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
-  Expect<Equal<If<false, 'a', 2>, 2>>,
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
 ]
 
 // @ts-expect-error
-type error = If<null, 'a', 'b'>
+type error = If<null, "a", "b">
