9c5b85c8e441b5913398afefc1b88c88d5b1e7a9
zubetcha <zuhye5@gmail.com>
2023-04-04 22:55:20 +0900

    easy: Push

--- .playground/03057-easy-push.ts
+++ .playground/03057-easy-push.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Push<T, U> = any
+type Push<T extends any[], U> = [...T, U]
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
