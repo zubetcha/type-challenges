0a4aab008617ce357d82bb766ab354f7ca8dccf2
zubetcha <zuhye5@gmail.com>
2023-04-05 22:37:10 +0900

    easy: Length of Tuple

--- .playground/00018-easy-length-of-tuple.ts
+++ .playground/00018-easy-length-of-tuple.ts
@@ -22,7 +22,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Length<T> = any
+type Length<T extends readonly any[]> = T['length']
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
