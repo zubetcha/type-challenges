d0b1e02a380fe2f68b717758e19d99b3dc97a91a
zubetcha <zuhye5@gmail.com>
2023-04-06 22:03:51 +0900

    eash: Unshift

--- .playground/03060-easy-unshift.ts
+++ .playground/03060-easy-unshift.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Unshift<T, U> = any
+type Unshift<T extends readonly any[], U> = [U, ...T]
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
