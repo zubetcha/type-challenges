99ae98bdf25d1b7e99da61950e8263f6d6d8432f
zubetcha <zuhye5@gmail.com>
2023-04-08 22:42:09 +0900

    medium: Append to Argument

--- .playground/00191-medium-append-argument.ts
+++ .playground/00191-medium-append-argument.ts
@@ -23,7 +23,11 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends (...args: any[]) => any, A> = Fn extends (
+  ...args: infer P
+) => infer R
+  ? (...args: [...P, A]) => R
+  : never
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
