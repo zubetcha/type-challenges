fad6a5655e4addf2f12caaf603d8c55cd7bcb38f
zubetcha <zuhye5@gmail.com>
2023-05-16 22:44:25 +0900

    medium: Pop, PromiseAll

--- .playground/00016-medium-pop.ts
+++ .playground/00016-medium-pop.ts
@@ -26,16 +26,12 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = number
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = [
-  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
-  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
-  Expect<Equal<Pop<[]>, []>>,
-]
+type cases = [Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>, Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>, Expect<Equal<Pop<[]>, []>>]
 
 /* _____________ 다음 단계 _____________ */
 /*
--- .playground/00020-medium-promise-all.ts
+++ .playground/00020-medium-promise-all.ts
@@ -23,7 +23,32 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-declare function PromiseAll(values: any): any
+// readonly
+// promise.resolve
+
+type PromiseReturnType<T extends Readonly<any[]>, Array extends any[] = []> = T extends [infer First, ...infer Rest]
+  ? First extends Promise<Awaited<First>>
+    ? PromiseReturnType<Rest, [...Array, Awaited<First> extends number ? number : Awaited<First>]>
+    : PromiseReturnType<Rest, [...Array, First]>
+  : Promise<Array>
+
+type A = 'abc' // string
+type Num = 3 // number
+
+// declare function PromiseAll<T extends Readonly<any[]>>(values: T): PromiseReturnType<[...T]>
+declare function PromiseAll<T extends unknown[]>(
+  values: readonly [...T]
+): Promise<{ [P in keyof T]: T[P] extends Promise<infer R> ? R : T[P] }>
+
+type Test = PromiseReturnType<readonly [1, 2, 3]>
+type Test2 = readonly [1, 2, 3] extends [infer First, ...infer Rest] ? true : false
+
+type Tuple = [1, 2, 3]
+type Test200<T extends any[]> = { [P in keyof T]: T[P] }
+type TT = Promise<{ [P in keyof Tuple]: Tuple[P] }>
+type Testtt = Expect<Equal<Tuple, Test200<Tuple>>>
+
+type TestTest = Promise<{ '0': 0; '1': 1; '2': 2; '3': 3; length: 4 }>
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -37,7 +62,7 @@ type cases = [
   Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
   Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
   Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
-  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>,
+  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>
 ]
 
 /* _____________ 다음 단계 _____________ */
